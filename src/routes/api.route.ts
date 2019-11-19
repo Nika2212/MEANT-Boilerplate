66ca314ae6ee8122c64b2b3f6afa3da733489c16
Nika Barsukovi <n.barsukovi@betsolutions.com>
2019-11-19 18:44:45 +0400

    user Routes

--- src/routes/api.route.ts
+++ src/routes/api.route.ts
@@ -3,7 +3,12 @@ import { IBaseRoute } from "../core/IBase.route";
 
 export class ApiRoute extends BaseRoute implements IBaseRoute {
     public route(): BaseRoute {
-        
+        this.express.route('/api/users').get(this.userController.index.bind(this));
+        this.express.route('/api/user/create').post(this.userController.create.bind(this));
+        this.express.route('/api/user/show/:id').get(this.userController.show.bind(this));
+        this.express.route('/api/user/update/:id').put(this.userController.update.bind(this));
+        this.express.route('/api/user/destroy/:id').delete(this.userController.destroy.bind(this));
+
         return this;
     }
 }
